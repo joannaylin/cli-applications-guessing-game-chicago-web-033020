ddf95ecb38db61fcf0019c06e6a2c60319cab91f
Joanna Lin <lin.joanna.y@gmail.com>
2020-03-25 03:16:06 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -10,3 +10,13 @@ end
 def user_input
   input = gets.chomp
 end
+
+def check
+  if input == random_number
+    return "You guessed the correct number!"
+  elsif input == "exit"
+    return "Goodbye!"
+  else
+    return "Sorry! The computer guessed #{random_number}"
+    
+end
