1ca49070ff592e6911dd3e615e9ebc41e97f66a2
Joanna Lin <lin.joanna.y@gmail.com>
2020-03-25 03:22:19 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   random_number = (rand(6) + 1)
   puts "Please guess a number between 1 and 6."
   input = gets.chomp
-  if input == random_number.to_s
+  if input == random_number.to_s #input is a string, so random_number needs to be converted to a string to match
     puts "You guessed the correct number!"
   elsif input == "exit"
     puts "Goodbye!"
